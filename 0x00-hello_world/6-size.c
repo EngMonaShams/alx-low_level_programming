a777d4c4cc28eeeafc72bb83b9acb786977f40a6
engmonashams <monashams247@gmail.com>
2023-07-13 15:18:16 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints sizes of different data type
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d",size of (char)" byte(s)"); 
+        return (0);
+}
