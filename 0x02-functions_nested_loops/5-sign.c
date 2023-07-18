540a4ed2f21d1adb424581caf2ca813bbabbed2d
engmonashams <monashams247@gmail.com>
2023-07-18 03:43:03 -0700

    Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - a prototype that prints sign of a number
+ *
+ *@n: check input of function
+ *
+ * Return: 1 (positive number)
+ *         0 (zero)
+ *         -1 (negative)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
