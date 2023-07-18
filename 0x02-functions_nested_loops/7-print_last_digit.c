525764b1ffb23bdf24567b5d66fb190a6c63fef0
engmonashams <monashams247@gmail.com>
2023-07-18 05:24:02 -0700

    Task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - a prototype that prints last digit
+ *
+ *@i: input of function
+ *
+ * Return: i
+ */
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+	return (i);
+}
