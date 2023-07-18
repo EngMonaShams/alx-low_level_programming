e9a48c83a0fa5f6322bb0e95a3547be7d18337f2
engmonashams <monashams247@gmail.com>
2023-07-18 06:35:51 -0700

    Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - a prototype that adds two int
+ *
+ *@a: first int
+ *@b: second int
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
