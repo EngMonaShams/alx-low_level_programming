45026c133eed309032f21918e21eb2184bdc59d0
engmonashams <monashams247@gmail.com>
2023-07-18 06:53:33 -0700

    Task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a prototype that prints natural numbers
+ *
+ *@n: input of function
+ *
+ */
+
+void print_to_98(int n)
+{
+	int k;
+
+	if (n < 98)
+	{
+		for (k = n; k < 98; k++)
+			printf("%d, ", k);
+	}
+	if (n > 98)
+	{
+		for (k = n; k > 98; k--)
+			printf("%d, ", k);
+	}
+	printf("%d", 98);
+
+
+}
