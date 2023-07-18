9edc032ee9893ee7a73777d1f8e9f513444bb050
engmonashams <monashams247@gmail.com>
2023-07-18 06:07:14 -0700

    Task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  times_table- a prototype that prints the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int t, n, pr;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			_putchar(' ');
+			if (pr / 10 == 0)
+				_putchar(' ');
+			else
+				_putchar('0' + pr / 10);
+			_putchar('0' + pr % 10);
+			_putchar(',');
+		}
+	}
+}
