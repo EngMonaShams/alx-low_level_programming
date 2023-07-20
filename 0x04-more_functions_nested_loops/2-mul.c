ab0c10580131425d6516b83cbc24bb960ef8639e
engmonashams <monashams247@gmail.com>
2023-07-20 05:56:16 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @a : first int
+ * @b : second int
+ *
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
