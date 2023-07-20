0e37cb72663b2229a2d46a0f52611c3cda1ace50
engmonashams <monashams247@gmail.com>
2023-07-20 16:12:20 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints the numbers, from 0 to 9
+ */
+
+void more_numbers(void)
+{
+	int i, j, n;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			n = i;
+			if (i > 9)
+			{
+				_putchar(49);
+				n = i % 10;
+			}
+		_putchar('0' + n);
+		}
+	_putchar('\n');
+	}
+}
