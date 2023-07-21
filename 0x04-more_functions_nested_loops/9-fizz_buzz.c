e82af107a1f161811edd6dce459236e108a23787
engmonashams <monashams247@gmail.com>
2023-07-20 17:15:28 -0700

    Task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", 1);
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
-			printf("Fizz ");
+			printf(" Fizz");
 		else if ((i % 3 != 0) && (i % 5 == 0))
-			printf("Buzz ");
+			printf(" Buzz");
 		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 	return (0);
