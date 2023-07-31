cdb2d210d1ad02585d78f0414de294be323c60c4
engmonashams <monashams247@gmail.com>
2023-07-31 07:36:05 -0700

    Task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ *
+ * @haystack: pointer to string
+ * @needle: string of string
+ *
+ * Return: pointer of char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *ptr;
+	int i, count = 0, flag;
+
+	ptr = needle;
+
+	while (*ptr)
+	{
+		count++;
+		ptr++;
+	}
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			flag = 1;
+			for (i = 0; i <= count; i++)
+				if (haystack[i] != needle[i])
+				{
+					flag = 0;
+					break;
+				}
+			if (flag == 1)
+				return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
