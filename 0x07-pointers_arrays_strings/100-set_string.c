6917a4705474d26c72c642709721c890e87f7984
engmonashams <monashams247@gmail.com>
2023-07-31 15:28:05 -0700

    Task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - a function that  fills memory with a constant byte.
+ *
+ * @s: pointer topointer
+ * @to: pointer to string
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
