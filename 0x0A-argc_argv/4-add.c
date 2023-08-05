f416d6622d95ce8b5c4adb2843a54f0cc3713829
engmonashams <monashams247@gmail.com>
2023-08-04 18:40:17 -0700

    Task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - prints prog name
+ *
+ * @argc: counter
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+
+	while (--argc)
+	{
+		if (*argv[argc] >= '0' && *argv[argc] <= '9')
+			sum += atoi(argv[argc]);
+		else
+			return (printf("Error\n"), 1);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
