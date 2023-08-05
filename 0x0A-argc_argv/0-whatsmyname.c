f5b7bba3e66b483a8bd7f8d869ac1ca2ce47518b
engmonashams <monashams247@gmail.com>
2023-08-04 17:29:59 -0700

    Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints prog name
+ *
+ * @argc: counter
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
