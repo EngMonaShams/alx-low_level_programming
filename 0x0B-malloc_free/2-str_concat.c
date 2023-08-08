77d57833cc34e2d4a5954b7cd6dc4d20b1ee4b8c
engmonashams <monashams247@gmail.com>
2023-08-08 10:17:21 -0700

    Task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 	int size1 = 0, size2 = 0, i;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	for (i = 0; s1[size1] != '\0'; size1++)
 		;
 	for (i = 0; s2[size2] != '\0'; size2++)
