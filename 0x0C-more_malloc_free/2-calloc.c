92a3b229f0b2fdc280ec03545a90561846fd7d7b
engmonashams <monashams247@gmail.com>
2023-08-10 16:12:08 -0700

    Task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * _memset - a function that ...
+ *
+ * @ptr: pointer to mem
+ * @b: set byte
+ * @count: number of bytes
+ *
+ * Return: pointer m
+ */
+
+void *_memset(char *ptr, char b, int count)
+{
+	char *s;
+
+	s = ptr;
+	while (count--)
+		*s++ = b;
+	return (ptr);
+}
+
+
+/**
  * _calloc - a function that ...
  *
  * @nmemb: number of elements
@@ -12,12 +33,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *m;
+	void *m;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = calloc(
+	m = malloc(nmemb * size);
 	if (m == 0)
-		exit(98);
+		return (NULL);
+	_memset(m, 0, nmemb * size);
 	return (m);
 }
