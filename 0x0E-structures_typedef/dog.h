81677c35d79734277c36dc4e0494761e20d727a6
engmonashams <monashams247@gmail.com>
2023-08-14 05:48:26 -0700

    Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: its a dog structure 
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H*/
