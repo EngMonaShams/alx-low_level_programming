f450dfb18f8a4413957a2db51f90b5d1dc7fc175
engmonashams <monashams247@gmail.com>
2023-08-14 03:47:07 -0700

    commit

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _OBJECT_LIKE_MARCO.H
-#define _OBJECT_LIKE_MARCO.H
+#ifndef _OBJECT_LIKE_MARCO_H
+#define _OBJECT_LIKE_MARCO_H
 
 #define SIZE 1024
 
-#endif
+#endif /* _OBJECT_LIKE_MARCO_H */
