263c05a1b90056c36d9c1f104aedfac2e6f7409e
engmonashams <monashams247@gmail.com>
2023-08-14 03:39:34 -0700

    commit

---
+++ 0x0D-preprocessor/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints last digit of a random number
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n, d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10;
+
+	if (d == 0)
+		printf("Last digit of %d is %d and is 0\n", n, d);
+	else if (d > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, d);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+	return (0);
+}
