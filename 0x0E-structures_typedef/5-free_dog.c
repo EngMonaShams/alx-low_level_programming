b210bb2447172e6403c7d6cf9cca0743eb4c0d0b
engmonashams <monashams247@gmail.com>
2023-08-14 09:04:59 -0700

    Task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * free_dog - a function that free dog data
+ *
+ * @d: pointer
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
