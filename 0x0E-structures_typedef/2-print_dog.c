6ff6e9dbc57796a8602b01ba3033fa5bcfab8b50
engmonashams <monashams247@gmail.com>
2023-08-14 06:20:38 -0700

    Task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <dog.h>
+
+/**
+ * print_dog - afunction that prints dog data
+ *
+ * @d: pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age ? d->age : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
