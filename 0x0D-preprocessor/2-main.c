c330d3ac0d64faf311be879f59578305eb34b4a9
engmonashams <monashams247@gmail.com>
2023-08-14 03:54:15 -0700

    commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints file name
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
