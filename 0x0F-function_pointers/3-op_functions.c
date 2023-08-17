3cf9c1ab4cb4b2bce6bfb2c3ec495379bcaf5d9b
engmonashams <monashams247@gmail.com>
2023-08-16 17:49:37 -0700

    Task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function that add two ints
+ *
+ * @a : int
+ * @b : int
+ *
+ * Return: Result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function that add two ints
+ *
+ * @a : int
+ * @b : int
+ *
+ * Return: Result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that add two ints
+ *
+ * @a : int
+ * @b : int
+ *
+ * Return: Result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that add two ints
+ *
+ * @a : int
+ * @b : int
+ *
+ * Return: Result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that add two ints
+ *
+ * @a : int
+ * @b : int
+ *
+ * Return: Result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
