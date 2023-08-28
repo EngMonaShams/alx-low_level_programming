d4b2513deaa866321ade66dbad64c41bd5f3a039
engmonashams <monashams247@gmail.com>
2023-08-28 15:53:24 -0700

    Task

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,13 +15,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node, *prev_node;
 	unsigned int i = 0;
 
-	if (!head || !new_node)
+	if (!head || !*head)
 		return (-1);
 	if (!index)
 	{
 		node = *head;
 		*head = (*head)->next;
-		free(node)
+		free(node);
 		return (1);
 	}
 	node = *head;
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (1);
 		}
 		i++;
-		prev_node = node
+		prev_node = node;
 		node = node->next;
 	}
 	return (-1);
