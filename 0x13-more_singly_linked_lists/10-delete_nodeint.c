79b34cfc1862c079fbd6a3b32eac50b0517b755f
engmonashams <monashams247@gmail.com>
2023-08-28 15:47:54 -0700

    Task

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - a function that inserts a new node
+ *
+ * @head: pointer to first node
+ * @index: index of the node
+ *
+ * Return: 1 (success)
+ *        -1 (faliure)
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !new_node)
+		return (-1);
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node)
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node
+		node = node->next;
+	}
+	return (-1);
+}
