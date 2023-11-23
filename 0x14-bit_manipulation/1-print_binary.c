8bf6073f22f64ceb905057398b6487b2fe58e51c
engmonashams <monashams247@gmail.com>
2023-11-23 12:54:50 -0800

    Task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation
+ *
+ * @n: integer
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while ((mask << 1) && (n >= (mask << 1)))
+		mask <<= 1;
+	while (mask)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
+}
+
